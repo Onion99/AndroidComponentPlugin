d4a4aaf897cdf0b147b5b91e7867164b77535eb8
androidmalin <malin.myemail@163.com>
2024-07-06 22:32:41 +0800

    handle compile error

--- buildSrc/src/main/kotlin/KotlinConstants.kt
+++ buildSrc/src/main/kotlin/KotlinConstants.kt
@@ -4,11 +4,11 @@ object KotlinConstants {
 }
 
 object AppConfig {
-    const val COMPILE_SDK_VERSION = 35
-    const val BUILD_TOOLS_VERSION = "35.0.0"
+    const val COMPILE_SDK_VERSION = 34
+    const val BUILD_TOOLS_VERSION = "34.0.0"
     const val NDK_VERSION = "25.2.9519653"
     const val APPLICATION_ID = "com.malin.hook"
-    const val MIN_SDK_VERSION = 15
+    const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 35
     const val VERSION_CODE = 300
     const val VERSION_NAME = "300.0"
@@ -19,17 +19,17 @@ object DependenciesConfig {
 
     const val STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinConstants.KOTLIN_VERSION}"
 
-    const val APP_COMPAT = "androidx.appcompat:appcompat:1.7.0"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.6.1"
 
-    const val ANNOTATION = "androidx.annotation:annotation:1.8.0"
+    const val ANNOTATION = "androidx.annotation:annotation:1.6.0"
 
-    const val KTX_CORE = "androidx.core:core-ktx:1.13.1"
+    const val KTX_CORE = "androidx.core:core-ktx:1.10.1"
 
     const val ASYNC_LAYOUT = "androidx.asynclayoutinflater:asynclayoutinflater:1.0.0"
 
     const val HIDDEN_API_PASS = "org.lsposed.hiddenapibypass:hiddenapibypass:4.3"
 
-    const val MATERIAL = "com.google.android.material:material:1.12.0"
+    const val MATERIAL = "com.google.android.material:material:1.9.0"
 
     const val X_CRASH = "com.iqiyi.xcrash:xcrash-android-lib:3.1.0"
 }
